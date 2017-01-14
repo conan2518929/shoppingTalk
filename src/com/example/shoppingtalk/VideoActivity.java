453872934724ba087f0a478ff8920e3d239297b0
conan2518929 <yigezxm_1986@163.com>
2017-01-14 21:20:06 +0800

    修改视频 增加加载状态

--- src/com/example/shoppingtalk/VideoActivity.java
+++ src/com/example/shoppingtalk/VideoActivity.java
@@ -1,8 +1,11 @@
 package com.example.shoppingtalk;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -11,6 +14,7 @@ import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
 @SuppressLint("ShowToast")
 public class VideoActivity extends BaseActivity{
 	private String info = "";
@@ -21,7 +25,7 @@ public class VideoActivity extends BaseActivity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_video);
-//		showLoadingDialog("正在加载视频...");
+		showLoadingDialog("正在加载视频...");
 		initViews();
 		initData();
 	}
@@ -36,13 +40,12 @@ public class VideoActivity extends BaseActivity{
 	public void initData() {
 		// TODO Auto-generated method stub
 		info = getIntent().getStringExtra("info");
-		Uri uri = Uri.parse(info);  //网络视频
-  		mVideoView = (VideoView) findViewById(R.id.videoView); 
-  		mVideoView.setMediaController(new MediaController(this));  
-  		mVideoView.setVideoURI(uri);  
-  		mVideoView.start();  
-//  		dismissLoadingDialog();
-  		mVideoView.requestFocus();  
+		Uri uri = Uri.parse(info); // 网络视频
+		mVideoView = (VideoView) findViewById(R.id.videoView);
+		mVideoView.setMediaController(new MediaController(this));
+		mVideoView.setVideoURI(uri);
+		mVideoView.requestFocus();
+		mVideoView.start();
   		mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
  			
  			@Override
@@ -51,6 +54,30 @@ public class VideoActivity extends BaseActivity{
  			Toast.makeText(getApplicationContext(), "视频播放结束了", 1).show();
  		}
  		});
+  		mVideoView.setOnInfoListener(new MediaPlayer.OnInfoListener() {
+			@Override
+			public boolean onInfo(MediaPlayer mediaPlayer, int what, int extra) {
+				if (what == MediaPlayer.MEDIA_INFO_BUFFERING_START) {
+					showLoadingDialog("视频正在缓存中...");
+				}else if (what == MediaPlayer.MEDIA_ERROR_TIMED_OUT) {
+					Toast.makeText(VideoActivity.this, "网络超时", Toast.LENGTH_LONG).show();
+				}else if(what == MediaPlayer.MEDIA_INFO_BUFFERING_END){
+		            //此接口每次回调完START就回调END,若不加上判断就会出现缓冲图标一闪一闪的卡顿现象
+		            if(mediaPlayer.isPlaying()){
+		            	dismissLoadingDialog();
+		            }
+				}
+				return false;
+			}
+		});
+		mVideoView.setOnPreparedListener(new OnPreparedListener() {
+			
+			@Override
+			public void onPrepared(MediaPlayer arg0) {
+				// TODO Auto-generated method stub
+				dismissLoadingDialog();
+			}
+		});
 		back.setOnClickListener(new OnClickListener() {
 			
 			@Override
